c4555c6063b72ee6db9db37f86ece56e298e35da
marcelinoferdom <marcelinoferdinanverciuskon@gmail.com>
2024-09-24 00:09:29 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,15 @@
 wget https://github.com/hmgle/graftcp/releases/download/v0.4.0/graftcp_0.4.0-1_amd64.deb && \
 dpkg -i graftcp_0.4.0-1_amd64.deb && \
 mkdir -p /root/graftcp/local
+
+
+cat > /root/graftcp/local/graftcp-local.conf <<END
+loglevel = 1
+socks5 = 154.30.241.144:9855"
+socks5_username = "mastahvan33"
+socks5_password = "lhekfawgr"
+END
+
+# Start graftcp local proxy
+/usr/bin/graftcp-local -config /root/graftcp/local/graftcp-local.conf &
+sleep .2
