354a9f8dbc422ce16e6212bad36595d605b94d1d
marcelinoferdom <marcelinoferdinanverciuskon@gmail.com>
2024-09-24 00:19:51 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-# Infinite while loop
-while true; do
-    # Get the current time in HH:MM:SS format
-    current_time=$(date +"%H:%M:%S")
-    
-    # Print the current time with the message
-    echo "[$current_time] This loop will run forever until you stop it."
+# Mulai Docker daemon
+dockerd &
 
-    # Check if the current second is 00, which means the minute has passed
-    if [ $(date +"%S") -eq 00 ]; then
-        # Generate a random 6-digit number
-        verification_code=$(shuf -i 100000-999999 -n 1)
-        
-        # Print the 6-digit verification code
-        echo "[$current_time] Verification Code: $verification_code"
-    fi
-    
-    # Optional: add a 1-second pause to prevent overwhelming the system
-    sleep 10
-done
+# Tunggu Docker daemon untuk siap
+sleep 5
+
+# Jalankan perintah yang diteruskan ke container
+exec "$@"
